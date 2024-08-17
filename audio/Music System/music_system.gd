0efc3fd43157206467d91de05ff51e9493d48d06
Seth Davis <sethhdavis99@gmail.com>
2024-08-17 11:38:23 -0400

    add music system fade

--- audio/Music System/music_system.gd
+++ audio/Music System/music_system.gd
@@ -3,4 +3,5 @@ extends Node
 @onready var layers = [null,$Small,$Mid,$Base]
 
 func toggle_music(index, music_on):
-	layers[index].volume_db = -80 * int(!music_on)
+	var tween = get_tree().create_tween()
+	tween.tween_property(layers[index],"volume_db",-80.0 * float(!music_on),1).set_trans(Tween.TRANS_LINEAR)
