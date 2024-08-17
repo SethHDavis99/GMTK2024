48f944c9ce5de1e98df26b7212ced7321dfa1140
Seth Davis <sethhdavis99@gmail.com>
2024-08-17 13:26:08 -0400

    fix bug where levels wont progress

--- scripts/exit_door.gd
+++ scripts/exit_door.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @export var required_size := 1
-@export_file("*.tscn") var next_level
+@export var next_level : PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,14 +9,10 @@ func _ready() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player and body.size == required_size:
-		if FileAccess.file_exists(next_level):
-			Global.play_sound(preload("res://audio/GMTK2024_DollGoalReach_02.ogg"),global_position)
-			$Timer.start()
-			get_tree().paused = true
-		else:
-			printerr("ERROR: Next level does not exist!")
-
+		Global.play_sound(preload("res://audio/GMTK2024_DollGoalReach_02.ogg"),global_position)
+		$Timer.start()
+		get_tree().paused = true
 
 func _on_timer_timeout() -> void:
-	get_tree().call_deferred("change_scene_to_file",next_level)
+	get_tree().call_deferred("change_scene_to_packed",next_level)
 	get_tree().paused = false
