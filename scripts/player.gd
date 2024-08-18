3cba95e9d34ab6d61fafe42566801921b41a995a
carnol16 <74794392+carnol16@users.noreply.github.com>
2024-08-17 21:25:40 -0400

    Player Fall VFX

--- scripts/player.gd
+++ scripts/player.gd
@@ -33,8 +33,9 @@ func _physics_process(delta: float) -> void:
 	elif velocity.y == 0:
 		if not landed:
 			footstep = Global.play_sound(preload("res://audio/GMTK2024_FootStep_02.ogg"),global_position,[0.8,1.2])
+			#$FallingVFX/GPUParticles2D.restart()
 			$FallingVFX/GPUParticles2D.emitting = true
-			$FallingVFX/GPUParticles2D2.emitting = true
+			#$FallingVFX/GPUParticles2D2.emitting = true
 			$AnimatedSprite2D.play("IDLE")
 		coyote_finished = false
 		$CoyoteTime.stop()
