cfb1473bad450b195e024ce4f1d9bb4672198e50
Seth Davis <sethhdavis99@gmail.com>
2024-08-17 22:08:09 -0400

    add credits music and background

--- audio/Music System/music_system.gd
+++ audio/Music System/music_system.gd
@@ -3,5 +3,11 @@ extends Node
 @onready var layers = [null,$Small,$Mid,$Base]
 
 func toggle_music(index, music_on):
-	var tween = get_tree().create_tween()
-	tween.tween_property(layers[index],"volume_db",-80.0 * float(!music_on),1).set_trans(Tween.TRANS_LINEAR)
+	if layers[index]:
+		var tween = get_tree().create_tween()
+		tween.tween_property(layers[index],"volume_db",-80.0 * float(!music_on),1).set_trans(Tween.TRANS_LINEAR)
+
+func stop_all():
+	for i in layers.size():
+		if i:
+			toggle_music(i,false)
